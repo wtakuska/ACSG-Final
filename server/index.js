ab6801e2231330a6644dd1b4f98b07e7eb37ba7d
Wioleta Takuska <takuska.w01@mymail.sxu.edu>
2022-07-25 17:16:42 -0500

    Updated index.js, schema_file.js, package-lock.json and package.json

--- server/index.js
+++ server/index.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
+const dotenv = require('dotenv');
 const { Server } = require("socket.io");
+const { default: mongoose } = require("mongoose");
+
+dotenv.config();
+
+app.use(express.json());
 
 app.use(cors());
 
@@ -15,6 +21,50 @@ const io = new Server(server, {
     },
 });
 
+mongoose.connect(process.env.DB_ACCESS, () => console.log("Database connected")).catch(() => console.log("Database connection error"));
+
+var schema = require('./schema_file').userSchema;
+
+const User = mongoose.model(
+    "User",
+    schema,
+    "usernames"
+);
+
+mongoose.connection.once('open', function(){
+    console.log('Connected to database!')
+    app.use('/', express.query());
+
+    app.get('/Users', function(request, response){  
+        //var test = request.query.name;
+        var query = User.find(request.query.uniqueName);
+        query.exec(function (err, docs){
+            console.log(docs);
+            response.status(200);
+            response.send(JSON.stringify({docs}));
+        });
+    })
+
+    app.post('/Users', function(request, response){
+        var newUser = new User({
+            uniqueName: request.body.uniqueName
+        });
+        newUser.save(function (err, doc) {
+            console.log(doc);
+            response.status(200);
+        });
+            
+    })
+
+    app.listen(5005, function (){
+        console.log("server listening on port 5005");
+    })
+
+});
+
+var userNames = [];
+
+
 //listening for events within socket.io server
 io.on("connection", (socket) => {
     console.log(`Connected: ${socket.id}`);
@@ -26,6 +76,17 @@ io.on("connection", (socket) => {
 
     socket.on("send_message", (data) => {
         socket.to(data.room).emit("obtain_message", data);
+    });
+
+    socket.on('username', () => {
+        if(userNames[username]) {
+            console.log(`User: ${username} exists in DB!`);
+        } else {
+            userNames[username] = username;
+            socket.username = username;
+            io.sockets.emit('usernames', userNames)
+        }
+
     });
 
     socket.on("disconnect", () => {
--- server/schema_file.js
+++ server/schema_file.js
@@ -1,14 +1,4 @@
-const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
-const dotenv = require('dotenv');
-//var async = require('async')
-
-dotenv.config()
-
-mongoose.connect(process.env.DB_ACCESS, () => console.log("Database connected")).catch(() => console.log("Database connection error"));
-// const connectDB = mongoose.connect("mongodb+srv://<username>:<password>@cluster0.3kcz7.mongodb.net/?retryWrites=true&w=majority"
-// ).then(() => console.log("Database connection successful")).catch(() => console.log("Database connection error"));
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -17,56 +7,4 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-const User = mongoose.model(
-    "User",
-    userSchema,
-    "usernames"
-);
-
-
-User.findOne({username: "Violet"}, (err, foundItem) => {
-    if (err){
-        console.log(err);    
-    }else {
-        console.log(foundItem);
-    }
-})
-
-app.listen(5005, function (){
-    console.log("server listening on port 5005");
-})
-
-//const mongoURI = "mongodb+srv://<username>:<password>@cluster0.3kcz7.mongodb.net/?retryWrites=true&w=majority"
-// notice the mongoose.createConnection instead of mongoose.connect
-// const conn = mongoose.createConnection(mongoURI);
-
-/*mongoose.connect(mongoURI, { useNewUrlParser: true , useUnifiedTopology: true});
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));*/
-
-
-function create(chatUserName) {
-    User.create([
-        { username: chatUserName}
-    ]);
-
-    // var awesome_instance = new User({username: 'Violet', roomID: 'general'})
-
-    // awesome_instance.save(function (err) {
-    //     if (err){
-    //         console.log(err); 
-    //     }
-    //     console.log('New Author: ' + awesome_instance);
-    //   } );
-}
-
-function getAll() {
-    const filter = {username: 'Violet'};
-    const all = User.findOne(filter);
-    console.log(all);
-    return all;
-}
-
-// create('Violet');
-getAll();
-// db.close();
+exports.userSchema = userSchema;
